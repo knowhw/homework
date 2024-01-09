f3ef069842510a7a44592490314dddfd2cd55119
knowhw <142631271+knowhw@users.noreply.github.com>
2024-01-09 05:36:51 +0000

    Create xrange.py

---
+++ xrange.py
@@ -0,0 +1,99 @@
+
+
+
+__python__ = "2.7+"
+__school__ = "istanbul.edu.tr"
+__github__ = "@knowhw"
+
+
+
+class get:
+	class item:
+		def array(
+		*_):
+			try:
+				return _  [: ] [ -1 % 1 ]  [ (_) [ -1] ]
+			except:
+				return None
+				
+	class values:
+		z = int(chr(48))
+
+def xrange(start, stop, step=.1):
+	step = float(step)
+	
+	"""
+ 
+  	xrange(*_) -> list of float
+	    xrange(start, stop[, step]) -> list of float
+
+	    For example, xrange(0., .3, .1) returns [0.0, 0.1, 0.2, 0.3].  The end point is not omitted!
+	  
+
+ 																			"""
+
+	arr=[]
+	push=arr.append
+	
+	
+	
+	_ =(not isinstance(wt, (float, int)) for wt in (start, stop))
+	
+
+	if any(_): 
+		
+		return []
+	 											
+
+	for ree in range( int(start), (int(stop) + 1), (1) ):
+		
+	
+		_ =(step if step else .0 * _ ^ _ * 0. ) 
+		
+		
+		if _:
+			 
+			if not arr: 
+				push(float(start)) 
+			
+			else:
+				if  (ree / step * step == ree):
+					z = get.values.z
+					
+					if round(ree - get.item.array(arr, -1), 1) % step == z:
+						
+						push(float(ree)) 
+				else:
+					pass
+			
+				
+		ef = (.1, 
+			  .2, 
+			  .3, 
+			  .4, 
+			  .5, 
+			  .6, 
+			  .7, 
+			  .8, .9)
+		
+		for auz in ef:
+
+			(__, ) =  sum([ree, auz,]), 
+
+			lite = get.item.array(arr, -1)
+
+			if .8:
+				
+				if auz > start - ree and (__ )<= stop:
+					
+					if round((   __)- lite, 1 ) % step == 0: push((__))
+
+
+			
+			else:
+				pass
+
+							
+	return arr 
+
+
