ed2532bb01cb969bdfe6bf4a21c7f99b7d05f2e2
knowhw <142631271+knowhw@users.noreply.github.com>
2024-02-13 21:05:52 -0600

    Update __init__.py

--- library-management/management/__init__.py
+++ library-management/management/__init__.py
@@ -1,7 +1,7 @@
 
-
 class Library:
-
+	def __init__(self):
+		self.books = open("books.txt", "a+")
 
 	def add_book(self, attr, count=10):
 		# input -> attr: title, author, published, pages
@@ -9,7 +9,7 @@ class Library:
 		title, author, published, pages = attr
 		
 		
-		with open("books.txt", "a+") as string:
+		with self.books as string:
 			string.write("%s,%s,%s,%s\n" % (title, author, published, pages))
 			# yeni kayit ve son satira bir bos satir ekle
 			
@@ -24,13 +24,16 @@ class Library:
 
 	def list_book(self):
 		
-
-		for line in open("books.txt").read().splitlines():
-			
+		books = self.books.read().splitlines()
+		
+		for line in books:
+		
 			title, author, published, pages = line.split(chr(44))
 			print("#", title, '"%s"' % author)
-			
-			
+		if not books:
+			print("You have got no books yet!")
+
+	
 	def save(self, string):
 		string = string.strip()
 		# alt satirdaki bos satirlari sil
@@ -41,7 +44,7 @@ class Library:
 		# main -> input: title
 		
 		
-		arr = [ [item] for item in open("books.txt").read().splitlines() if item ]
+		arr = [ [item] for item in self.books.read().splitlines() if item ]
 		# son satirdaki bos satirlari listeye alma
 		for index, line in enumerate(arr):
 			
@@ -52,8 +55,9 @@ class Library:
 		
 		for line in arr : 
 			if line:
+				# bos satirlari atla
 				string += '%s\n' % ''.join(line)
-		
+				
 		self.save(string)
 				
 				
